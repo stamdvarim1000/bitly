67baabb8a1cef1481227a90ba82e194fcde5c450
stamdvarim1000 <stamdvarim1000@gmail.com>
2022-04-05 20:14:56 +0300

    add layout

---
+++ scripts/clearShit.bat
@@ -0,0 +1,1 @@
+find /path/to/dir -empty -type d -delete
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,8 +5,21 @@ import './Layout.css';
 
 export const Layout = () => {
     return (
-        <div>
-            Layout
+        <div className='layout'>
+            <div className='board'>
+
+                <div className="header">
+                    <div className="header-text">
+                        <h1>header</h1>
+                    </div>
+                    <div className="business-image">
+                        img
+                    </div>
+                </div>
+                <div className="welcome-text">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae quo maxime voluptas officia. Impedit tenetur delectus sit perferendis incidunt porro cumque accusamus dolores culpa beatae, autem magnam id adipisci illum!
+                </div>
+            </div>
         </div>
     );
 }
